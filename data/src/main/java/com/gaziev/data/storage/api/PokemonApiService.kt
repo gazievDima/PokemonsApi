70765af5306505e7d5a16c5606c2a24dcbc768c9
gazievDima <dmtr.cw@gmail.com>
2022-04-11 00:25:37 +0300

    add empty

--- data/src/main/java/com/gaziev/data/storage/api/PokemonApiService.kt
+++ data/src/main/java/com/gaziev/data/storage/api/PokemonApiService.kt
@@ -5,18 +5,7 @@ import com.gaziev.data.repository.source.INetworkApi
 
 class PokemonApiService : INetworkApi {
     override fun getPokemons(): List<PokemonApi> {
-        return listOf(
-            PokemonApi("Покемон 1"),
-            PokemonApi("Покемон 2"),
-            PokemonApi("Покемон 3"),
-            PokemonApi("Покемон 4"),
-            PokemonApi("Покемон 5"),
-            PokemonApi("Покемон 6"),
-            PokemonApi("Покемон 7"),
-            PokemonApi("Покемон 8"),
-            PokemonApi("Покемон 9"),
-            PokemonApi("Покемон 10"),
-        )
+        return emptyList()
     }
 
     override fun getPokemonsPage(pageNumber: Int, pageSize: Int): List<PokemonApi> {
--- data/src/main/java/com/gaziev/data/storage/room/PokemonRoomDataBase.kt
+++ data/src/main/java/com/gaziev/data/storage/room/PokemonRoomDataBase.kt
@@ -13,18 +13,7 @@ class PokemonRoomDataBase : ILocalDataBase {
     }
 
     override fun getPokemons(): List<PokemonApi> {
-        return listOf(
-            PokemonApi("Покемон 1"),
-            PokemonApi("Покемон 2"),
-            PokemonApi("Покемон 3"),
-            PokemonApi("Покемон 4"),
-            PokemonApi("Покемон 5"),
-            PokemonApi("Покемон 6"),
-            PokemonApi("Покемон 7"),
-            PokemonApi("Покемон 8"),
-            PokemonApi("Покемон 9"),
-            PokemonApi("Покемон 10"),
-        )
+        return emptyList()
     }
 
 }
